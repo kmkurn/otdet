8e5ad2138d0685e4322156b3f545be46a3f0c99f
Kemal Maulana <kemskems12@gmail.com>
2014-10-22 20:06:59 +0700

    Modify to pick either randomly or sequentially

--- util.py
+++ util.py
@@ -3,10 +3,15 @@
 import glob
 import os.path
 import random
+import re
 
 
-def pick_random(directory, k=None):
-    """Pick randomly some files from a directory."""
+def pick(directory, k=None, randomized=True):
+    """Pick some thread files from a thread directory."""
     all_files = glob.glob(os.path.join(directory, '*'))
-    random.shuffle(all_files)
+    if randomized:
+        random.shuffle(all_files)
+    else:
+        pattern = '([0-9]+)\.txt'
+        all_files.sort(key=lambda f: int(re.search(pattern, f).group(1)))
     return all_files if k is None else all_files[:k]
