adfb8a0b8f9c038e10d85b0844213e524f0bf568
Kemal Maulana <kemskems12@gmail.com>
2014-10-22 20:50:54 +0700

    Fix to pass unit tests

--- util.py
+++ util.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
-import glob
-import os.path
 import random
 import re
 
 
-def pick(directory, k=None, randomized=True):
+def pick(filenames, k=None, randomized=True):
     """Pick some thread files from a thread directory."""
-    all_files = glob.glob(os.path.join(directory, '*'))
     if randomized:
-        random.shuffle(all_files)
+        random.shuffle(filenames)
     else:
         pattern = '([0-9]+)\.txt'
-        all_files.sort(key=lambda f: int(re.search(pattern, f).group(1)))
-    return all_files if k is None else all_files[:k]
+        filenames.sort(key=lambda f: int(re.search(pattern, f).group(1)))
+    return filenames if k is None else filenames[:k]
