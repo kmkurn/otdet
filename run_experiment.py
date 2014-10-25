13935bbcdc06b384a92ca37e82d6c7105272c722
Kemal Maulana <kemskems12@gmail.com>
2014-10-25 11:11:51 +0700

    Refactor and accomodate multiple experiments

--- run_experiment.py
+++ run_experiment.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     from termcolor import cprint
 
     from method import OffTopicDetector
+    from util import Evaluator
     import util
 
     parser = argparse.ArgumentParser(description='Run experiment '
@@ -17,44 +18,68 @@ if __name__ == '__main__':
     parser.add_argument('oot_thread', type=str,
                         help='Thread directory from which '
                         'OOT post will be taken')
-    parser.add_argument('-m', type=int, default=None,
-                        help='Number of posts taken from normal_thread')
-    parser.add_argument('-n', type=int, default=None,
-                        help='Number of posts taken from oot_thread')
-    parser.add_argument('--method', type=str, default='clust_dist', nargs='*',
+    parser.add_argument('--num-norm', type=int, default=None,
+                        help='Number of posts taken from '
+                        'normal thread directory')
+    parser.add_argument('--num-oot', type=int, default=None,
+                        help='Number of posts taken from '
+                        'another thread directory to be OOT posts')
+    parser.add_argument('--method', type=str, nargs='+',
                         choices=['clust_dist', 'mean_comp', 'txt_comp_dist'],
                         help='OOT post detection method to use')
+    parser.add_argument('-N', type=int, default=1,
+                        help='Number of posts in top N list')
+    parser.add_argument('--niter', type=int, default=1,
+                        help='Number of iteration for each method')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Be verbose')
     args = parser.parse_args()
 
-    # Obtain normal posts
-    normfiles = util.pick(glob(os.path.join(args.normal_thread, '*')),
-                          k=args.m, randomized=False)
-    print('Obtaining', len(normfiles), 'normal posts')
-    for file in normfiles:
-        cprint(file, 'green')
-
-    # Obtain OOT posts
-    ootfiles = util.pick(glob(os.path.join(args.oot_thread, '*')), k=args.n)
-    print('Obtaining', len(ootfiles), 'OOT posts:')
-    for file in ootfiles:
-        cprint(file, 'red')
-
-    # Combine them both
-    files = normfiles + ootfiles
-    truth = [False]*len(normfiles) + [True]*len(ootfiles)
-
-    # Apply OOT post detection methods
-    detector = OffTopicDetector(files)
     for meth in args.method:
-        print('\nApplying', meth, 'method...', end=' ')
-        methodfunc = getattr(detector, meth)
-        res = methodfunc()
-        print('OK')
+        print('### Applying', meth, 'method ###')
+        evaluator = Evaluator(args.N)
+        for ii in range(args.niter):
+            # Obtain normal posts
+            normfiles = util.pick(glob(os.path.join(args.normal_thread, '*')),
+                                  k=args.num_norm, randomized=False)
+            # Print obtained normal posts in verbose mode
+            if args.verbose:
+                print('Obtaining', len(normfiles), 'normal posts')
+                for file in normfiles:
+                    cprint(file, 'green')
+            # Obtain OOT posts
+            ootfiles = util.pick(glob(os.path.join(args.oot_thread, '*')),
+                                 k=args.num_oot)
+            # Print obtained OOT posts in verbose mode
+            if args.verbose:
+                print('Obtaining', len(ootfiles), 'OOT posts:')
+                for file in ootfiles:
+                    cprint(file, 'red')
+            # Combine them both
+            files = normfiles + ootfiles
+            truth = [False]*len(normfiles) + [True]*len(ootfiles)
 
-        # Construct ranked list of OOT posts (1: most off-topic)
-        ranked = reversed(sorted(zip(files, res, truth), key=lambda x: x[1]))
+            # Apply OOT post detection methods
+            print('Iteration #{:02}:'.format(ii+1), end=' ')
+            detector = OffTopicDetector(files)
+            methodfunc = getattr(detector, meth)
+            distances = methodfunc()
+            print('OK')
 
-        # Print result
-        for i, (file, score, t) in enumerate(ranked):
-            txt = '#{:02} {} -> {}'.format(i+1, file, score)
-            cprint(txt, 'red' if t else 'green')
+            # Construct ranked list of OOT posts (1: most off-topic)
+            # In case of tie, prioritize normal post (worst case)
+            s = sorted(zip(files, distances, truth), key=lambda x: x[2])
+            ranked = sorted(s, key=lambda x: x[1], reverse=True)
+
+            # Print result in verbose mode
+            if args.verbose:
+                for i, (file, distance, oot) in enumerate(ranked):
+                    txt = '#{:02} {} -> {}'.format(i+1, file, distance)
+                    cprint(txt, 'red' if oot else 'green')
+
+            # Store ranked list for evaluation
+            ranked = [(distance, oot) for _, distance, oot in ranked]
+            evaluator.add_result(ranked)
+
+        print('Baseline:', evaluator.baseline)
+        print('Performance:', evaluator.get_performance)
