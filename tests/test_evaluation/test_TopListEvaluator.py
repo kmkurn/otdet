969ba90c837ac3b116fbb739cf725f9fa1918f46
Kemal Maulana <kemskems12@gmail.com>
2014-10-25 21:12:20 +0700

    Again, add more tests

--- tests/test_evaluation/test_TopListEvaluator.py
+++ tests/test_evaluation/test_TopListEvaluator.py
@@ -1,4 +1,4 @@
-from nose.tools import assert_equal, assert_greater
+from nose.tools import assert_equal, assert_greater, raises
 from numpy.testing import assert_almost_equal
 import numpy as np
 
@@ -46,8 +46,6 @@ class TestBaseline:
     def setUp(self):
         self.sample_result = [(5.0, True), (4.0, False), (3.0, True),
                               (2.0, False), (1.0, False)]
-        self.M = len(self.sample_result)
-        self.n = sum(elm[1] for elm in self.sample_result)
 
     def test_normal_case(self):
         N = 3
@@ -69,3 +67,29 @@ class TestBaseline:
         evaluator.add_result(self.sample_result)
         expected = np.array([0.0, 0.4, 0.6])
         assert_almost_equal(evaluator.baseline, expected)
+
+
+class TestGetPerformance:
+    def setUp(self):
+        sample_result1 = zip(range(5, 0, -1),
+                             [True, False, False, True, False])
+        sample_result2 = zip(range(5, 0, -1),
+                             [True, False, True, False, False])
+        sample_result3 = zip(range(5, 0, -1),
+                             [False, True, True, False, False])
+        sample_result4 = zip(range(5, 0, -1),
+                             [False, False, False, True, True])
+        self.result_list = [sample_result1, sample_result2,
+                            sample_result3, sample_result4]
+
+    def test_normal_case(self):
+        N = 3
+        evaluator = TopListEvaluator(N)
+        for sample_result in self.result_list:
+            evaluator.add_result(sample_result)
+        expected = np.array([0.25, 0.25, 0.50])
+        assert_almost_equal(evaluator.get_performance, expected)
+
+    @raises(Exception)
+    def test_no_experiment(self):
+        TopListEvaluator().get_performance
