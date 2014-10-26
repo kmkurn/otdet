d93f179a30215139ac4ba1057a44ae2a44b2089f
Kemal Maulana <kemskems12@gmail.com>
2014-10-26 09:15:04 +0700

    Crazy refactoring experiment script

--- run_experiment.py
+++ run_experiment.py
@@ -2,8 +2,11 @@
 
 if __name__ == '__main__':
     import argparse
+    from collections import defaultdict
     from glob import glob
+    import itertools as it
     import os.path
+    import sys
 
     from termcolor import cprint
 
@@ -18,82 +21,138 @@ if __name__ == '__main__':
     parser.add_argument('ootdir', type=str,
                         help='Thread directory from which '
                         'OOT post will be taken')
-    parser.add_argument('-m', '--num-norm', type=int, default=None,
+    parser.add_argument('-m', '--num-norm', type=int, nargs='+',
                         help='Number of posts taken from '
                         'normal thread directory')
-    parser.add_argument('-n', '--num-oot', type=int, default=None,
+    parser.add_argument('-n', '--num-oot', type=int, nargs='+',
                         help='Number of posts taken from '
                         'another thread directory to be OOT posts')
     parser.add_argument('-a', '--method', type=str, nargs='+',
                         choices=['clust_dist', 'mean_comp', 'txt_comp_dist'],
                         help='OOT post detection method to use')
-    parser.add_argument('-t', '--top', type=int, default=1,
+    parser.add_argument('-t', '--top', type=int, nargs='+',
                         help='Number of posts in top N list')
     parser.add_argument('--niter', type=int, default=1,
                         help='Number of iteration for each method')
-    parser.add_argument('-v', '--verbose', action='store_true',
+    parser.add_argument('-v', '--verbose', action='count',
                         help='Be verbose')
     parser.add_argument('-c', '--colorized', action='store_true',
                         help='Colorize output')
     args = parser.parse_args()
 
-    for meth in args.method:
-        print('##### Applying', meth, 'method #####')
-        evaluator = TopListEvaluator(args.top)
-        for ii in range(args.niter):
+    # Experiment settings
+    expr_settings = list(it.product(args.num_norm, args.num_oot, args.method,
+                                    args.top))
+
+    # Progress-related variables
+    total_ops = len(expr_settings) * args.niter
+    step, step_size, progress_count = 1, total_ops / 100, 0
+
+    report_dict = defaultdict(dict)
+    for ii, setting in enumerate(expr_settings):
+        num_norm, num_oot, method, top = setting
+        # Begin experiment
+        evaluator = TopListEvaluator(top)
+        report_dict[setting]['iteration'] = []
+        for jj in range(args.niter):
             # Obtain normal posts
             normfiles = pick(glob(os.path.join(args.normdir, '*.txt')),
-                             k=args.num_norm, randomized=False)
-            # Print obtained normal posts in verbose mode
-            if args.verbose:
-                print('>> Obtaining', len(normfiles), 'normal posts')
-                for file in normfiles:
-                    if args.colorized:
-                        cprint(file, 'green')
-                    else:
-                        print(file)
+                             k=num_norm, randomized=False)
             # Obtain OOT posts
             ootfiles = pick(glob(os.path.join(args.ootdir, '*.txt')),
-                            k=args.num_oot)
-            # Print obtained OOT posts in verbose mode
-            if args.verbose:
-                print('>> Obtaining', len(ootfiles), 'OOT posts:')
-                for file in ootfiles:
-                    if args.colorized:
-                        cprint(file, 'red')
-                    else:
-                        print(file)
+                            k=num_oot)
             # Combine them both
             files = normfiles + ootfiles
             truth = [False]*len(normfiles) + [True]*len(ootfiles)
 
             # Apply OOT post detection methods
-            print('Iteration #{:02}:'.format(ii+1), end=' ')
             detector = OOTDetector(files)
-            methodfunc = getattr(detector, meth)
+            methodfunc = getattr(detector, method)
             distances = methodfunc()
-            print('OK')
 
             # Construct ranked list of OOT posts (1: most off-topic)
             # In case of tie, prioritize normal post (worst case)
             s = sorted(zip(files, distances, truth), key=lambda x: x[2])
             ranked = sorted(s, key=lambda x: x[1], reverse=True)
 
-            # Print result in verbose mode
-            if args.verbose:
-                for i, (file, distance, oot) in enumerate(ranked):
-                    if args.colorized:
-                        txt = '#{:02} {} -> {}'.format(i+1, file, distance)
-                        cprint(txt, 'red' if oot else 'green')
-                    else:
-                        sym = 'o' if oot else ' '
-                        txt = '#{:02} {} {} -> {}'.format(i+1, sym, file,
-                                                          distance)
-                        print(txt)
+            # Put all to report
+            tup = (normfiles, ootfiles, ranked)
+            report_dict[setting]['iteration'].append(tup)
 
             # Store ranked list for evaluation
             ranked = [(distance, oot) for _, distance, oot in ranked]
             evaluator.add_result(ranked)
 
-        print('Baseline:', evaluator.baseline)
-        print('Performance:', evaluator.get_performance)
+            # Print progress to stderr
+            print('.', end='', file=sys.stderr, flush=True)
+            progress_count += 1
+            if progress_count >= step * step_size:
+                print('{}%'.format(step), end='', file=sys.stderr, flush=True)
+                step += 1
+
+        report_dict[setting]['baseline'] = evaluator.baseline
+        report_dict[setting]['performance'] = evaluator.get_performance
+
+    print(' Done', file=sys.stderr, flush=True)
+
+    # Print experiment report
+    print('Normal thread dir                :', args.normdir)
+    print('OOT thread dir                   :', args.ootdir)
+    if args.verbose == 1:
+        print('Number of normal posts           :', args.num_norm)
+        print('Number of OOT posts              :', args.num_oot)
+        print('OOT detection methods            :', ' '.join(args.method))
+        print('Number of posts in top list      :', args.top)
+        print('Number of iterations             :', args.niter)
+
+    print(len(expr_settings), 'experiment(s)')
+    print()
+
+    for ii, sett in enumerate(report_dict):
+        num_norm, num_oot, method, top = sett
+
+        # Preprocess num_norm
+        if num_norm < 0:
+            num_norm = len(glob(os.path.join(args.normdir, '*.txt')))
+
+        # Print experiment setting info
+        print('##### Experiment {} #####'.format(ii+1))
+        txt = '  M = {}, n = {}, N = {}, method = {}'
+        print(txt.format(num_norm+num_oot, num_oot, top, method))
+
+        # Print obtained normal posts in very verbose mode
+        if args.verbose == 2:
+            for normfiles, ootfiles, ranked in report_dict[sett]['iteration']:
+                print('    >> Obtaining', len(normfiles), 'normal posts')
+                for file in normfiles:
+                    txt = '    {}'.format(file)
+                    if args.colorized:
+                        cprint(txt, 'green')
+                    else:
+                        print(txt)
+
+                print('    >> Obtaining', len(ootfiles), 'OOT posts:')
+                for file in ootfiles:
+                    txt = '    {}'.format(file)
+                    if args.colorized:
+                        cprint(txt, 'red')
+                    else:
+                        print(txt)
+
+                print('    >> Result')
+                for i, (file, distance, oot) in enumerate(ranked):
+                    if args.colorized:
+                        txt = '    #{:02} {} -> {}'.format(i+1, file, distance)
+                        cprint(txt, 'red' if oot else 'green')
+                    else:
+                        sym = 'o' if oot else ' '
+                        txt = '    #{:02} {} {} -> {}'.format(i+1, sym, file,
+                                                              distance)
+                        print(txt)
+
+        baseline = ['{:.3f}'.format(p)
+                    for p in report_dict[sett]['baseline']]
+        performance = ['{:.3f}'.format(p)
+                       for p in report_dict[sett]['performance']]
+        print('  Baseline:', '  '.join(baseline))
+        print('  Performance:', '  '.join(performance))
