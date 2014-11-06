e649d1fe1b854582a2ac6399aecbf096e6a0b742
Kemal Maulana <kemskems12@gmail.com>
2014-11-06 15:05:42 +0700

    Write function to compute expected value

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,6 @@
-from otdet.util import pick
+from otdet.util import pick, expected
 
-from nose.tools import assert_equal, assert_true, raises
+from nose.tools import assert_equal, assert_almost_equal, assert_true, raises
 
 
 class TestPick():
@@ -31,3 +31,33 @@ class TestPick():
     @raises(Exception)
     def test_negative_k(self):
         pick(self.filenames, k=-2)
+
+
+class TestExpected():
+    def test_k_from_nonzero(self):
+        k = range(2, 6)
+        p = [0.25] * 4
+        assert_almost_equal(expected(k, p), 3.5)
+
+    def test_from_zero_k(self):
+        k = range(5)
+        p = [0.2] * 5
+        assert_almost_equal(expected(k, p), 2.0)
+
+    @raises(Exception)
+    def pmf_sums_greater_than_one(self):
+        k = range(3)
+        p = [0.5, 0.5, 0.3]
+        expected(k, p)
+
+    @raises(Exception)
+    def pmf_sums_less_than_one(self):
+        k = range(3)
+        p = [0.2, 0.3, 0.2]
+        expected(k, p)
+
+    @raises(Exception)
+    def negative_probability(self):
+        k = range(2)
+        p = [0.5, -0.5]
+        expected(k, p)
