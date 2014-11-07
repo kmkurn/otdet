b525249ffb7786193415c757448e2d135c65b874
Kemal Maulana <kemskems12@gmail.com>
2014-11-07 10:58:44 +0700

    Set storing to HDF5 format as default

--- run_experiment.py
+++ run_experiment.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import argparse
-from collections import defaultdict
 from glob import glob
 import itertools as it
 from multiprocessing import Pool
@@ -14,7 +13,7 @@ import pandas as pd
 
 from otdet.detector import OOTDetector
 from otdet.evaluation import TopListEvaluator
-from otdet.util import pick, expected
+from otdet.util import pick
 
 
 def experiment(setting):
@@ -90,8 +89,10 @@ if __name__ == '__main__':
                         help='Number of iteration for each method')
     parser.add_argument('-j', '--jobs', type=int, default=1,
                         help='Number of work processes')
-    parser.add_argument('-hdf', type=str,
-                        help='Directory to store the result as HDF5 format')
+    parser.add_argument('--hdf-name', type=str, required=True,
+                        help='Where to store the result in HDF5 format')
+    parser.add_argument('--hdf-key', type=str, default='df',
+                        help='Identifier in the HDF5 store')
     args = parser.parse_args()
 
     # Experiment settings
@@ -103,100 +104,49 @@ if __name__ == '__main__':
     with Pool(processes=args.jobs) as pool:
         results = pool.map_async(experiment, settings).get()
 
-    report = defaultdict(dict)
-    if args.hdf is not None:
-        index_tup, column_tup = [], []
-        data = np.array([])
+    index_tup, column_tup = [], []
+    data = np.array([])
     for setting, result in zip(settings, results):
         # Evaluate the result of each setting
         baseline, performance, min_sup, max_sup = evaluate(result, setting)
-        # Prepare storing the result in HDF5 format
-        if args.hdf is not None:
-            # Prepare Pandas MultiIndex tuples
-            (norm_dir, oot_dir, num_norm, num_oot,
-                method, metric, num_top) = setting
-            norm_dir = shorten(norm_dir)
-            oot_dir = shorten(oot_dir)
-            index_tup.append((norm_dir, oot_dir, method, metric))
-            for res in ['base', 'perf']:
-                for k in range(min_sup, max_sup+1):
-                    column_tup.append((num_norm, num_oot, num_top, res, k))
-            # Prepare Pandas DataFrame data
-            data = np.concatenate((data, baseline))
-            data = np.concatenate((data, performance))
-        # Store the report
-        report[setting]['baseline'] = baseline
-        report[setting]['performance'] = performance
-        report[setting]['min_sup'] = min_sup
-        report[setting]['max_sup'] = max_sup
 
-    # Store the result in HDF5 format
-    if args.hdf is not None:
-        # Create index tuples list
-        st = set()
-        index = []
-        for idx in index_tup:
-            if idx not in st:
-                index.append(idx)
-                st.add(idx)
-        # Create column tuples list
-        st = set()
-        columns = []
-        for col in column_tup:
-            if col not in st:
-                columns.append(col)
-                st.add(col)
-        index_names = ['norm_dir', 'oot_dir', 'method', 'metric']
-        column_names = ['num_norm', 'num_oot', 'num_top', 'result', 'k']
-        index = pd.MultiIndex.from_tuples(index, names=index_names)
-        columns = pd.MultiIndex.from_tuples(columns, names=column_names)
-        df = pd.DataFrame(data.reshape((len(index), len(columns))),
-                          index=index, columns=columns)
-        df.to_hdf(args.hdf, 'df')
-        print("Stored in HDF5 format with the name 'df'")
+        # Prepare Pandas MultiIndex tuples
+        (norm_dir, oot_dir, num_norm, num_oot,
+            method, metric, num_top) = setting
+        norm_dir = shorten(norm_dir)
+        oot_dir = shorten(oot_dir)
+        index_tup.append((norm_dir, oot_dir, method, metric))
+        for res in ['base', 'perf']:
+            for k in range(min_sup, max_sup+1):
+                column_tup.append((num_norm, num_oot, num_top, res, k))
+
+        # Prepare Pandas DataFrame data
+        data = np.concatenate((data, baseline))
+        data = np.concatenate((data, performance))
+
+    # Create index tuples list
+    st = set()
+    index = []
+    for idx in index_tup:
+        if idx not in st:
+            index.append(idx)
+            st.add(idx)
+    # Create column tuples list
+    st = set()
+    columns = []
+    for col in column_tup:
+        if col not in st:
+            columns.append(col)
+            st.add(col)
+    # Prepare to store in HDF5 format
+    index_names = ['norm_dir', 'oot_dir', 'method', 'metric']
+    column_names = ['num_norm', 'num_oot', 'num_top', 'result', 'k']
+    index = pd.MultiIndex.from_tuples(index, names=index_names)
+    columns = pd.MultiIndex.from_tuples(columns, names=column_names)
+    df = pd.DataFrame(data.reshape((len(index), len(columns))),
+                      index=index, columns=columns)
+    # Store in HDF5 format
+    df.to_hdf(args.hdf_name, args.hdf_key)
+    print("Stored in HDF5 format with the name '{}'".format(args.hdf_key))
 
     print('Done', file=sys.stderr, flush=True)
-
-    # Print experiment report
-    print('Normal thread dir                :')
-    for norm_dir in args.norm_dir:
-        print('  {}'.format(norm_dir))
-    print('OOT thread dir                   :')
-    for oot_dir in args.oot_dir:
-        print('  {}'.format(oot_dir))
-    print('Number of normal posts           :', args.num_norm)
-    print('Number of OOT posts              :', args.num_oot)
-    print('OOT detection methods            :', ' '.join(args.method))
-    print('Distance metrics                 :', ' '.join(args.metric))
-    print('Number of posts in top list      :', args.num_top)
-    print('Number of iterations             :', args.niter)
-
-    print(len(settings), 'experiment(s)')
-    print()
-
-    for ii, setting in enumerate(sorted(report)):
-        norm_dir, oot_dir, *rest = setting
-        baseline = report[setting]['baseline']
-        performance = report[setting]['performance']
-        min_sup = report[setting]['min_sup']
-        max_sup = report[setting]['max_sup']
-
-        # Print experiment setting info
-        if ii > 0 and ii % len(args.num_top) == 0:
-            print()
-        print('##### Experiment {} #####'.format(ii+1))
-        print('  norm_dir =', norm_dir)
-        print('  oot_dir =', oot_dir)
-        txt = '  m = {}, n = {}, method = {}, metric = {}, t = {}'
-        print(txt.format(*rest))
-
-        # Print experiment result summary
-        np.set_printoptions(precision=3, suppress=True,
-                            formatter={'float': '{:6.3f}'.format,
-                                       'int': '{:6d}'.format})
-        support = np.arange(min_sup, max_sup+1)
-        print('  SUPP\t\t:', support)
-        print('  BASE\t\t:', baseline)
-        print('  PERF\t\t:', performance)
-        print('  EXP BASE\t: {:.3f}'.format(expected(support, baseline)))
-        print('  EXP PERF\t: {:.3f}'.format(expected(support, performance)))
